8330d4dd295345d969bc14c30a9d043542e7d704
MaGaroo <amoomajid99@gmail.com>
2020-03-23 10:44:53 +0430

    Add default length to random string generator

--- src/events/utils.py
+++ src/events/utils.py
@@ -4,5 +4,5 @@ from string import ascii_uppercase, ascii_lowercase, digits
 random_charset = ascii_uppercase + ascii_lowercase + digits
 
 
-def generate_random_string(length):
+def generate_random_string(length=32):
     return ''.join(choice(random_charset) for _ in range(length))
