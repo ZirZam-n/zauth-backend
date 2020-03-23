bd57bb2209835ff6cd333fe6d6410d5b7511ad9e
MaGaroo <amoomajid99@gmail.com>
2020-03-23 10:23:24 +0430

    Add event model

--- src/events/models.py
+++ src/events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        unique=True,
+    )
+
+    public_key = models.CharField(
+        max_length=128,
+        null=False,
+        blank=False,
+        unique=True,
+    )
+
+    private_key = models.CharField(
+        max_length=128,
+        null=False,
+        blank=False,
+        unique=True,
+    )
