0be25b30f8784da74a5b2f0aa0525e3414b19b91
MaGaroo <amoomajid99@gmail.com>
2020-03-24 23:26:34 +0430

    Add city serializer

--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -1,7 +1,52 @@
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
-from accounts.models import Field, MajorField
+from accounts.models import Field, MajorField, City, Country, State
+
+
+class CitySerializer(serializers.ModelSerializer):
+    city = serializers.CharField(source='name')
+    state = serializers.CharField(source='state.name')
+    country = serializers.CharField(source='state.country.name')
+
+    class Meta:
+        model = City
+        fields = ['country', 'state', 'city']
+
+    def get_related_object(self, data, raise_exception=True):
+        country = Country.objects.filter(name=data.get('state').get('country').get('name'))
+        if country.count() != 1:
+            if raise_exception:
+                raise serializers.ValidationError('Country not found.')
+            return None
+        country = country.get()
+
+        state = country.state_set.filter(name=data.get('state').get('name'))
+        if state.count() != 1:
+            if raise_exception:
+                raise serializers.ValidationError('State not found.')
+            return None
+        state = state.get()
+
+        city = state.city_set.filter(name=data.get('name'))
+        if city.count() != 1:
+            if raise_exception:
+                raise serializers.ValidationError('City not found.')
+            return None
+        city = city.get()
+
+        return city
+
+    def validate(self, data):
+        self.get_related_object(data)
+        return data
+
+    def create(self, data):
+        return self.get_related_object(data)
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    pass
 
 
 class FieldSerializer(serializers.ModelSerializer):
