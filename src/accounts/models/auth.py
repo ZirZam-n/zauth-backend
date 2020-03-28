2fe8fb67fec23e2ee1758006a7d4d60e46dc9899
MaGaroo <amoomajid99@gmail.com>
2020-03-28 06:16:58 +0430

    Partition accounts app models into auth and profile parts

---
+++ src/accounts/models/auth.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
+from django.utils.translation import gettext as _
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+from accounts.models import EducationInfo, City
+
+
+class ZUser(AbstractUser):
+    blank_to_null = ['phone', 'nat_code', 'education', 'city']
+
+    phone = PhoneNumberField(
+        blank=True,
+        null=True,
+        default=None,
+        unique=True,
+    )
+
+    nat_code_validator = validators.RegexValidator(
+        regex=r'\d*',
+        message=_('National code must be a decimal sequence'),
+    )
+    nat_code = models.CharField(
+        validators=[nat_code_validator],
+        max_length=10,
+        blank=True,
+        null=True,
+        default=None,
+        unique=True,
+    )
+
+    education = models.OneToOneField(
+        EducationInfo,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        default=None,
+        related_name='user',
+    )
+
+    city = models.ForeignKey(
+        City,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        default=None,
+    )
+
+    avatar = models.ImageField()
+
+    def send_activation_email(self):
+        pass
+
+    def save(self, *args, **kwargs):
+        for field in ZUser.blank_to_null:
+            setattr(self, field, getattr(self, field) or None)
+        super(ZUser, self).save(*args, **kwargs)
--- src/accounts/models/profile.py
+++ src/accounts/models/profile.py
@@ -1,10 +1,6 @@
-from django.contrib.auth.models import AbstractUser
 from django.core import validators
 from django.utils.translation import gettext as _
 from django.db import models
-
-# Create your models here.
-from phonenumber_field.modelfields import PhoneNumberField
 
 
 class University(models.Model):
@@ -138,54 +134,3 @@ class City(models.Model):
 
     def __str__(self):
         return '{}:{}:{}'.format(self.state.country.name, self.state.name, self.name)
-
-
-class ZUser(AbstractUser):
-    blank_to_null = ['phone', 'nat_code', 'education', 'city']
-
-    phone = PhoneNumberField(
-        blank=True,
-        null=True,
-        default=None,
-        unique=True,
-    )
-
-    nat_code_validator = validators.RegexValidator(
-        regex=r'\d*',
-        message=_('National code must be a decimal sequence'),
-    )
-    nat_code = models.CharField(
-        validators=[nat_code_validator],
-        max_length=10,
-        blank=True,
-        null=True,
-        default=None,
-        unique=True,
-    )
-
-    education = models.OneToOneField(
-        EducationInfo,
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True,
-        default=None,
-        related_name='user',
-    )
-
-    city = models.ForeignKey(
-        City,
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True,
-        default=None,
-    )
-
-    avatar = models.ImageField()
-
-    def send_activation_email(self):
-        pass
-
-    def save(self, *args, **kwargs):
-        for field in ZUser.blank_to_null:
-            setattr(self, field, getattr(self, field) or None)
-        super(ZUser, self).save(*args, **kwargs)
