52000c01eb87474971c164af38077f46d290c2da
MaGaroo <amoomajid99@gmail.com>
2020-03-28 05:47:56 +0430

    Add future needed urls with None view function

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path(
         'login/',
         jwt_views.TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
+        name='token_obtain_pair',
     ),
     path(
         'register/',
@@ -16,23 +16,43 @@ urlpatterns = [
         name='register',
     ),
     path(
+        'activate/',
+        None,
+        name='activate_email',
+    ),
+    path(
+        'change_password/',
+        None,
+        name='change_password',
+    ),
+    path(
+        'edu/',
+        None,
+        name='edu_info',
+    ),
+    path(
+        'profile',
+        None,
+        name='profile',
+    ),
+    path(
         'refresh/',
         jwt_views.TokenRefreshView.as_view(),
-        name='token_refresh'
+        name='token_refresh',
     ),
     path(
         'field_list/',
         FieldsListView.as_view(),
-        name='field_list'
+        name='field_list',
     ),
     path(
         'city_list/',
         CityListView.as_view(),
-        name='city_list'
+        name='city_list',
     ),
     path(
         'university_list/',
         UniversityListView.as_view(),
-        name='university_list'
+        name='university_list',
     ),
 ]
