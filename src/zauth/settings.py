3780e12b7badbf269acf4e843a0fe9f6ebc954d3
MaGaroo <amoomajid99@gmail.com>
2020-03-28 07:13:41 +0430

    Add sites app to the project

--- src/zauth/settings.py
+++ src/zauth/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'phonenumber_field',
     'django_extensions',
@@ -136,3 +137,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+SITE_ID = 1
